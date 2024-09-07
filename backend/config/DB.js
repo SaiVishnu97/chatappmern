46e7c552e69c0ee765e86dbc670ad216d4aeffd0
Vishnu <vishnusai.kotte@gmail.com>
2024-09-07 19:56:23 +0530

    final commit

--- backend/config/DB.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose');
-
-async function connectDB()
-{
-    const connectionobj=await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB ${connectionobj.connection.host} is connected to the Backend`);
-}
-
-module.exports={connectDB}
